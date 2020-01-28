69c9723830f3695cc0d23492e2a4c9743584782d
Zivarg <bvjenia@gmail.com>
2020-01-28 17:07:14 +0300

    Создание базовой части проекта atina (rtiTest).

--- src/atinafd.cpp
+++ src/atinafd.cpp
@@ -1,38 +1,79 @@
 #include "atinafd.h"
 
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include <utility>
+
 namespace Atina {
 
-Fd::Fd(int aValue) : mValue{aValue} {}
+Fd::Fd(int aValue) : mName{defaultName()}, mValue{aValue} {}
 
-Fd::Fd(const Fd &aOther) : mValue{aOther.mValue} {}
+Fd::Fd(std::string aName, int aValue)
+    : mName{std::move(aName)}, mValue{aValue} {};
+
+Fd::Fd(const Fd &aOther) : mName{aOther.mName}, mValue{aOther.mValue} {}
 
 Fd &Fd::operator=(const Fd &aOther) {
   if (&aOther == this) {
     return *this;
   }
 
+  mName = aOther.mName;
+
   mValue = aOther.mValue;
 
   return *this;
 }
 
-Fd::~Fd() {
-  if (isValid(mValue) && close(mValue) != 0) {
-    fprintf(stderr, "File descriptor cannot be normally closed.");
-  }
-}
-
-int Fd::invalid() { return -1; }
-
-Fd::operator int() const {}
+Fd::operator int() const { return mValue; }
 
 Fd &Fd::operator=(int aValue) {
+  if (aValue == mValue) {
+    return *this;
+  }
+
   mValue = aValue;
 
   return *this;
 }
 
-bool Fd::isValid(int aValue) { return aValue >= 0; }
+bool Fd::validate(const int &aValue, const char *aName, bool aIsDisplayError) {
+  if (isValid(aValue)) {
+    return true;
+  }
+
+  if (aIsDisplayError) {
+    fprintf(stderr, "%s descriptor is not valid.",
+            aName != nullptr ? aName : defaultName());
+  }
+
+  return false;
+}
+
+bool Fd::validate(const int &aValue, const std::string &aName,
+                  bool aIsDisplayError) {
+  return validate(aValue, aName.c_str(), aIsDisplayError);
+}
+
+bool Fd::validate(const Fd &aFd, bool aIsDisplayError) {
+  return validate(aFd.mValue, aFd.mName, aIsDisplayError);
+}
+
+int Fd::isValid(int aValue) { return aValue >= 0; }
+
+void Fd::close(int &aValue, const char *aName) {
+  if (::close(aValue) != 0) {
+    fprintf(stderr, "%s cannot be normally closed.", aName);
+  }
+
+  aValue = invalid();
+}
+
+void Fd::close(Fd &aValue) { close(aValue.mValue, aValue.mName.c_str()); }
+
+const std::string &Fd::name() const { return mName; }
 
 bool Fd::isValid() const { return isValid(mValue); }
 
--- src/atinafd.h
+++ src/atinafd.h
@@ -1,38 +1,54 @@
 #ifndef ATINA_FD_H
 #define ATINA_FD_H
 
-#include <assert.h>
-
-#include <memory>
-
-#include <unistd.h>
+#include <string>
 
 namespace Atina {
 
 class Fd {
 public:
-  Fd() = default;
+  Fd(int aValue = invalid());
 
-  explicit Fd(int aValue);
+  Fd(std::string aName, int aValue = invalid());
 
   Fd(const Fd &aOther);
 
   Fd &operator=(const Fd &aOther);
 
-  ~Fd();
+  ~Fd() = default;
 
-  static int invalid();
-
-  static bool isValid(int aValue);
-
-  operator int() const;
+  explicit operator int() const;
 
   Fd &operator=(int aValue);
+
+  static constexpr const char *defaultName() { return "File descriptor"; }
+
+  static constexpr int invalid() { return -1; }
+
+  static bool validate(const int &aValue, const char *aName,
+                       bool aIsDisplayError = true);
+
+  static bool validate(const int &aValue, const std::string &aName,
+                       bool aIsDisplayError = true);
+
+  static bool validate(const Fd &aFd, bool aIsDisplayError = true);
+
+  static int isValid(int aValue);
+
+  static void close(int &aValue, const char *aName = defaultName());
+
+  static void close(int &aValue, const std::string &aName = defaultName());
+
+  static void close(Fd &aFd);
+
+  const std::string &name() const;
 
   bool isValid() const;
 
 private:
-  int mValue{invalid()};
+  std::string mName;
+
+  int mValue;
 };
 
 } // namespace Atina
